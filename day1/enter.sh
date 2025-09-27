7b49a5dd975aed9c9da00bdcb141c9f3ca0eb4d0
Ujjwal Panda <ujjwalpanda97@gmail.com>
2025-09-28 04:49:37 +0530

    Fix Dockerfile for Day 1.

---
+++ day1/enter.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Run the container.
+run() {
+    if [ -z "$(docker ps -f "name=day1" -f "status=running" -q)" ]; then
+        docker run \
+            -d \
+            -it \
+            --rm \
+            --name day1 \
+            --hostname day1 \
+	    -e DISPLAY=$DISPLAY \
+            -v /tmp/.X11-unix:/tmp/.X11-unix \
+            astrogewgaw/ftsky:day1 \
+            /bin/bash
+    fi
+}
+
+# Enter the container.
+enter() { docker exec -it day1 bash -i; }
+
+run && enter
