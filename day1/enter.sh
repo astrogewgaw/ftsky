62695893373aeb8fd8ba61a1071ea8ce73817ce6
Ujjwal Panda <ujjwalpanda97@gmail.com>
2025-09-28 04:52:00 +0530

    Format some files.

--- day1/enter.sh
+++ day1/enter.sh
@@ -2,18 +2,18 @@
 
 # Run the container.
 run() {
-    if [ -z "$(docker ps -f "name=day1" -f "status=running" -q)" ]; then
-        docker run \
-            -d \
-            -it \
-            --rm \
-            --name day1 \
-            --hostname day1 \
-	    -e DISPLAY=$DISPLAY \
-            -v /tmp/.X11-unix:/tmp/.X11-unix \
-            astrogewgaw/ftsky:day1 \
-            /bin/bash
-    fi
+  if [ -z "$(docker ps -f "name=day1" -f "status=running" -q)" ]; then
+    docker run \
+      -d \
+      -it \
+      --rm \
+      --name day1 \
+      --hostname day1 \
+      -e DISPLAY=$DISPLAY \
+      -v /tmp/.X11-unix:/tmp/.X11-unix \
+      astrogewgaw/ftsky:day1 \
+      /bin/bash
+  fi
 }
 
 # Enter the container.
--- day2A/enter.sh
+++ day2A/enter.sh
@@ -2,18 +2,18 @@
 
 # Run the container.
 run() {
-    if [ -z "$(docker ps -f "name=day2A" -f "status=running" -q)" ]; then
-        docker run \
-            -d \
-            -it \
-            --rm \
-            --name day2A \
-            --hostname day2A \
-	    -e DISPLAY=$DISPLAY \
-	    -v /tmp/.X11-unix:/tmp/.X11-unix \
-	    astrogewgaw/ftsky:day2A \
-            /bin/bash
-    fi
+  if [ -z "$(docker ps -f "name=day2A" -f "status=running" -q)" ]; then
+    docker run \
+      -d \
+      -it \
+      --rm \
+      --name day2A \
+      --hostname day2A \
+      -e DISPLAY=$DISPLAY \
+      -v /tmp/.X11-unix:/tmp/.X11-unix \
+      astrogewgaw/ftsky:day2A \
+      /bin/bash
+  fi
 }
 
 # Enter the container.
